f42d09368236e06e756b556170399e5b12f7c94e
Yoga Kurniawan <yoga.kwn@gmail.com>
2018-02-06 14:24:17 +0700

    add error page

---
+++ pages/hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="red">Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  asyncData (context) {
+    // called every time before loading the component
+    return { name: 'World' }
+  },
+  fetch () {
+    // The `fetch` method is used to fill the store before rendering the page
+  }
+  // head () {
+    // Set Meta Tags for this Page
+  // }
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
