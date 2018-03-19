8ec0e61da04c7ca3fdfaa0bb86aefaf5d85bef9c
Yoga Kurniawan <yoga.kwn@gmail.com>
2018-03-19 21:46:09 +0700

    upgrade nuxt

---
+++ pages/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <template v-for="variant in ['primary','secondary','success','outline-success','warning','danger','link']">
+      <div class="col-md-4 pb-2" v-for="size in ['sm','','lg']" :key="`${variant}_${size}`">
+        <b-button :size="size" :variant="variant">
+          {{variant}} {{size}}
+        </b-button>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
