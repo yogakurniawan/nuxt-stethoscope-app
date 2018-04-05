12578f40a370471fab492e05ffb60506db7928d0
Yoga Kurniawan <yoga.kwn@gmail.com>
2018-04-05 16:58:18 +0700

    new utils for updating query string parameter

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,15 @@
-export default function ({ $axios, redirect }) {
+import { updateQueryStringParameter } from '../utils/request'
+
+export default function ({ $axios, redirect, ...rest }) {
+  console.log(rest)
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
     if (code === 401) {
       console.log(code)
     }
+  })
+  $axios.onRequest(config => {
+    console.log(config)
+    config.url = updateQueryStringParameter(config.url, 'auth', 'foo')
   })
 }
